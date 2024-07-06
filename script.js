3d2cd93bf83270e4c227c9aabd159115efce12cc
Ashita MV <96426324+ashitamv@users.noreply.github.com>
2024-07-07 02:22:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('light-mode');
+    });
+});
